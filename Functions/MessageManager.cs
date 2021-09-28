37c29f8108a3aba21268d75a0e17944b2104ff23
VAC Efron <46462862+VACEfron@users.noreply.github.com>
2021-09-28 13:19:04 +0200

    Added MessageManager

---
+++ Functions/MessageManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+
+namespace QFighterPolice.Functions
+{
+    public static class MessageManager
+    {
+        public static async Task SendStatusUpdateMessageAsync(ITextChannel channel, bool isServerOnline)
+        {
+            var unixTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            var header = "__**Server status update**__ " + (isServerOnline ? "<:miublush2:845314172480782406>" : "<:miuscream3:845316873662496838>");
+            var body = $"**Timestamp:** <t:{unixTimestamp}>\n**Status:** {(isServerOnline ? "Online 🟢" : "Offline 🔴")}";
+
+            await channel.SendMessageAsync($"{header}\n\n{body}");
+        }
+    }
+}
