fb72fcad543856e984ea511ee6aad334c4afe1fb
VAC Efron <46462862+VACEfron@users.noreply.github.com>
2021-10-06 00:49:50 +0200

    Added try catch

--- Modules/Ping.cs
+++ Modules/Ping.cs
@@ -44,18 +44,25 @@ namespace QFighterPolice.Modules
 
         private static bool TryConnect(JObject config, uint attempts = 5)
         {
-            using var tcpClient = new TcpClient();
-            bool success = false;
-
-            for (int i = 0; i < attempts && !success; i++)
+            try
             {
-                var result = tcpClient.BeginConnect((string)config["server_ip"], (int)config["server_port"], null, null);
-                success = result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(3));
+                using var tcpClient = new TcpClient();
+                bool success = false;
 
-                Logger.LogMessage($"Server pinged. Attempt {i + 1} {(success ? "successful" : "failed")}.");
+                for (int i = 0; i < attempts && !success; i++)
+                {
+                    var result = tcpClient.BeginConnect((string)config["server_ip"], (int)config["server_port"], null, null);
+                    success = result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(3));
+
+                    Logger.LogMessage($"Server pinged. Attempt {i + 1} {(success ? "successful" : "failed")}.");
+                }
+
+                return success;
             }
-
-            return success;
+            catch
+            {
+                return false;
+            }
         }
     }
 }
